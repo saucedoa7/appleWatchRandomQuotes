fd55c0f4e79ec8902dde04b935e9687ca9c0c60e
Albert Saucedo <albert@Alberts-Mac-mini.local>
2014-11-28 19:37:55 -0600

    Initial Commit

---
+++ SwiftAppleWatch1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftAppleWatch1
+//
+//  Created by Albert Saucedo on 11/28/14.
+//  Copyright (c) 2014 Albert Saucedo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
